a655776dabf4b21dfbdd5aeefd3735fd8e5476a0
Christoph Jabs <contact@christophjabs.info>
2025-01-30 10:07:10 +0200

    deploy script

--- shell.nix
+++ shell.nix
@@ -1,15 +1,38 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [
-      nix
-      home-manager
-      git
+  default = let
+    deploy =
+      pkgs.writeScriptBin "deploy"
+      /*
+      bash
+      */
+      ''
+        hosts="$1"
+        shift
 
-      sops
-      ssh-to-age
-      gnupg
-      age
-    ];
-  };
+        if [ -z "$hosts" ]; then
+            echo "No hosts to deploy"
+            exit 2
+        fi
+
+        for host in ''${hosts//,/ }; do
+            ${pkgs.lib.getExe pkgs.nixos-rebuild} --flake .\#$host switch --target-host $host --use-remote-sudo
+        done
+      '';
+  in
+    pkgs.mkShell
+    {
+      NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+      nativeBuildInputs = with pkgs; [
+        nix
+        home-manager
+        git
+
+        sops
+        ssh-to-age
+        gnupg
+        age
+
+        deploy
+      ];
+    };
 }
