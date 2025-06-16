63459ab61e9abf5331161f73992aff1f0e320780
Christoph Jabs <christoph.jabs@helsinki.fi>
2025-06-16 14:46:40 +0300

    desktop portal for screen sharing etc

--- home/christoph/features/desktop/sway/default.nix
+++ home/christoph/features/desktop/sway/default.nix
@@ -131,4 +131,12 @@
       );
     };
   };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-wlr];
+    config.sway = {
+      default = ["wlr" "gtk"];
+    };
+  };
 }
