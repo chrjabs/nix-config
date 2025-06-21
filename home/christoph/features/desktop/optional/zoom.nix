9d727cc3d31ed793d02b48a1eb69f7fbefbe7bbf
Christoph Jabs <contact@christophjabs.info>
2025-06-21 15:05:51 +0300

    fix zoom persistence

--- home/christoph/features/desktop/optional/zoom.nix
+++ home/christoph/features/desktop/optional/zoom.nix
@@ -5,5 +5,5 @@
 }: {
   home.packages = with pkgs; [zoom-us];
 
-  home.persistence."/persist/${config.home.homeDirectory}".files = [".config/zoomus.conf"];
+  home.persistence."/persist/${config.home.homeDirectory}".files = [".config/zoom.conf"];
 }
