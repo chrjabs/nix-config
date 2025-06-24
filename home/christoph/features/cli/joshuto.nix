4a7bea34561b5de60e48260162c6c52b27c81ba1
Christoph Jabs <contact@christophjabs.info>
2025-06-24 11:17:36 +0300

    update system

--- home/christoph/features/cli/joshuto.nix
+++ home/christoph/features/cli/joshuto.nix
@@ -7,35 +7,6 @@
   programs.joshuto = {
     enable = true;
 
-    package = pkgs.rustPlatform.buildRustPackage rec {
-      pname = "joshuto";
-      version = "0.9.9";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "kamiyaa";
-        repo = pname;
-        rev = "v${version}";
-        hash = "sha256-hfu3Verbrq0to3I5/gX6ZhVr7ewjHNamzvaUcmcUIRU=";
-      };
-      cargoHash = "sha256-K/++/NdOLSvhxQ8LBS+jnthCRJxScoOjWSp7pmfHVaQ=";
-
-      nativeBuildInputs = [pkgs.installShellFiles];
-
-      postInstall = lib.optionalString (pkgs.stdenv.buildPlatform.canExecute pkgs.stdenv.hostPlatform) ''
-        installShellCompletion --cmd joshuto \
-          --bash <($out/bin/joshuto completions bash) \
-          --zsh <($out/bin/joshuto completions zsh) \
-          --fish <($out/bin/joshuto completions fish)
-      '';
-
-      meta = {
-        description = "Ranger-like terminal file manager written in Rust";
-        homepage = "https://github.com/kamiyaa/joshuto";
-        license = lib.licenses.lgpl3Only;
-        mainProgram = "joshuto";
-      };
-    };
-
     settings = {
       zoxide_update = true;
       cmd_aliases = {
