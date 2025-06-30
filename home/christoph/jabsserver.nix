83055ea33284e939d504d7d1b6feae4fc5956b7b
Christoph Jabs <contact@christophjabs.info>
2025-06-30 21:14:06 +0300

    fix jabsserver home config

--- home/christoph/jabsserver.nix
+++ home/christoph/jabsserver.nix
@@ -3,5 +3,17 @@
     ./global
   ];
 
-  home.persistence."/persist/${config.home.homeDirectory}".enable = false;
+  home = {
+    homeDirectory = "/tank/home/${config.home.username}";
+
+    persistence."/persist/${config.home.homeDirectory}".enable = false;
+  };
+
+  # No dconf
+  stylix.targets = {
+    gtk.enable = false;
+    gnome.enable = false;
+    gnome-text-editor.enable = false;
+    eog.enable = false;
+  };
 }
