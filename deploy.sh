fe88fc46db45185ae0a95545739c9069771528f0
Christoph Jabs <contact@christophjabs.info>
2025-08-05 17:54:15 +0300

    treewide: format

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 export NIX_SSHOPTS="-A"
 
-build_remote=false
-
 hosts="$1"
 shift
 
 if [ -z "$hosts" ]; then
-    echo "No hosts to deploy"
-    exit 2
+  echo "No hosts to deploy"
+  exit 2
 fi
 
 for host in ${hosts//,/ }; do
-    nixos-rebuild --flake .\#$host switch --target-host $host --sudo --use-substitutes $@
+  nixos-rebuild --flake ".#$host" switch --target-host "$host" --sudo --use-substitutes "$@"
 done
