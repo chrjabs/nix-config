66747dffdced56f70d838f532276c294e0f96087
Christoph Jabs <contact@christophjabs.info>
2025-08-10 06:03:04 +0300

    feat(terangreal): file server

--- hosts/terangreal/services/default.nix
+++ hosts/terangreal/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./file-server.nix
     ./headscale.nix
   ];
 }
---
+++ hosts/terangreal/services/file-server.nix
@@ -0,0 +1,19 @@
+let
+  files = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".root = "/srv/files/christophjabs";
+  };
+in
+{
+  services.nginx.virtualHosts = {
+    "media.christophjabs.info" = files;
+    "m.christophjabs.info" = files;
+    "files.christophjabs.info" = files;
+    "f.christophjabs.info" = files;
+  };
+
+  environment.persistence = {
+    "/persist".directories = [ "/srv/files" ];
+  };
+}
