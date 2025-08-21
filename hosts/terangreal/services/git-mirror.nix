e6d46e828ff8336be57c5b867d8fd7788e38044c
Christoph Jabs <contact@christophjabs.info>
2025-08-21 09:34:11 +0300

    feat(git): mirror more repos

--- hosts/terangreal/services/git-mirror.nix
+++ hosts/terangreal/services/git-mirror.nix
@@ -11,6 +11,15 @@
       nix-config = "https://github.com/chrjabs/nix-config";
       rustsat = "https://github.com/chrjabs/rustsat";
       scuttle = "https://github.com/chrjabs/scuttle";
+      nur-packages = "https://github.com/chrjabs/nur-packages";
+      "chrjabs.github.io" = "https://github.com/chrjabs/chrjabs.github.io";
+      gbd = "https://github.com/chrjabs/gbd";
+      gbdc = "https://github.com/chrjabs/gbdc";
+      dblp-helper = "https://github.com/chrjabs/dblp-helper";
+      Grape-Academic-Theme = "https://github.com/chrjabs/Grape-Academic-Theme";
+      maxpre-rs = "https://github.com/chrjabs/maxpre-rs";
+      MoaMopb = "https://github.com/chrjabs/MoaMopb";
+      masters-thesis = "https://github.com/chrjabs/masters-thesis";
     };
   };
 }
