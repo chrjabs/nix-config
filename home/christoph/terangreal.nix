67721d87a22092b114e73ec087f2a3cd669a8fd2
Christoph Jabs <contact@christophjabs.info>
2025-11-08 12:54:43 +0200

    feat(terangreal): set flake to github

--- home/christoph/terangreal.nix
+++ home/christoph/terangreal.nix
@@ -4,5 +4,8 @@
     ./global
   ];
 
-  home.persistence."/persist/${config.home.homeDirectory}".enable = false;
+  home = {
+    persistence."/persist/${config.home.homeDirectory}".enable = false;
+    sessionVariables.NH_FLAKE = "github:chrjabs/nix-config/terangreal";
+  };
 }
