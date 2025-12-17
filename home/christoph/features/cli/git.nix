85631ce47cef244621fd88223f1bbce509a20a21
Christoph Jabs <christoph.jabs@helsinki.fi>
2025-12-17 11:05:40 +0200

    feat: git authentication for overleaf

--- home/christoph/features/cli/git.nix
+++ home/christoph/features/cli/git.nix
@@ -10,39 +10,51 @@
     git = {
       enable = true;
       package = pkgs.gitFull;
-      settings = {
-        aliases = {
-          fpush = "push --force-with-lease";
-          p = "pull --ff-only";
-          ff = "merge --ff-only";
-          graph = "log --decorate --oneline --graph";
-        };
-        user = {
-          name = "Christoph Jabs";
-          email = if workMode then "christoph.jabs@helsinki.fi" else "contact@christophjabs.info";
-        };
-        init.defaultBranch = "main";
-        user.signing.key = "217C6A439646D51E";
-        commit.gpgSign = lib.mkDefault true;
-        gpg.program = "${config.programs.gpg.package}/bin/gpg2";
-
-        merge.conflictStyle = "zdiff3";
-        commit.verbose = true;
-        diff.algorithm = "histogram";
-        log.date = "iso";
-        column.ui = "auto";
-        branch.sort = "committerdate";
-        push.autoSetupRemote = true;
-        rerere.enabled = true;
-        "filter \"dumpsql\"" =
-          let
-            sqlite = lib.getExe pkgs.sqlite;
-          in
-          {
-            clean = "tmp=$(mktemp); cat > $tmp; ${sqlite} $tmp .dump; rm $tmp";
-            smudge = "tmp=$(mktemp); ${sqlite} $tmp; cat $tmp; rm $tmp";
+      settings =
+        let
+          pass = lib.getExe config.programs.password-store.package;
+          passCredentialHelper = path: ''!f() { test "$1" = get && echo "password=$(${pass} ${path})"; }; f'';
+        in
+        {
+          aliases = {
+            fpush = "push --force-with-lease";
+            p = "pull --ff-only";
+            ff = "merge --ff-only";
+            graph = "log --decorate --oneline --graph";
           };
-      };
+          user = {
+            name = "Christoph Jabs";
+            email = if workMode then "christoph.jabs@helsinki.fi" else "contact@christophjabs.info";
+          };
+          init.defaultBranch = "main";
+          user.signing.key = "217C6A439646D51E";
+          commit.gpgSign = lib.mkDefault true;
+          gpg.program = "${config.programs.gpg.package}/bin/gpg2";
+          "credential \"https://git.overleaf.com\"" =
+            let
+            in
+            {
+              username = "git";
+              helper = passCredentialHelper "git.overleaf.com/chrjabs";
+            };
+
+          merge.conflictStyle = "zdiff3";
+          commit.verbose = true;
+          diff.algorithm = "histogram";
+          log.date = "iso";
+          column.ui = "auto";
+          branch.sort = "committerdate";
+          push.autoSetupRemote = true;
+          rerere.enabled = true;
+          "filter \"dumpsql\"" =
+            let
+              sqlite = lib.getExe pkgs.sqlite;
+            in
+            {
+              clean = "tmp=$(mktemp); cat > $tmp; ${sqlite} $tmp .dump; rm $tmp";
+              smudge = "tmp=$(mktemp); ${sqlite} $tmp; cat $tmp; rm $tmp";
+            };
+        };
       lfs.enable = true;
       ignores = [
         "**/.ropeproject/"
