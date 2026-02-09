26d61e9c34c9d85c47b01defc6b1404372f6f111
Christoph Jabs <christoph.jabs@helsinki.fi>
2026-02-09 14:26:24 +0200

    fix(reverse-proxy): use ips to not block startup

--- hosts/terangreal/services/reverse-proxy.nix
+++ hosts/terangreal/services/reverse-proxy.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   basicReverseProxy = proxyPass: extraConfig: {
     enableACME = true;
@@ -13,21 +14,22 @@ let
   };
 in
 {
+  # Use tailnet IPs rather than hostnames to not prevent startup if not connected
   services.nginx.virtualHosts = {
-    "house.jabsserver.net" = basicReverseProxy "http://jabsserver:8123" ''
+    "house.jabsserver.net" = basicReverseProxy "http://100.64.0.3:8123" ''
       proxy_set_header Upgrade $http_upgrade;
       proxy_set_header Connection $connection_upgrade;
     '';
-    "home.jabsserver.net" = basicReverseProxy "http://rumah:8123" ''
+    "home.jabsserver.net" = basicReverseProxy "http://100.64.0.10:8123" ''
       proxy_set_header Upgrade $http_upgrade;
       proxy_set_header Connection $connection_upgrade;
     '';
-    "paper.jabsserver.net" = basicReverseProxy "http://paperless:8000" "";
-    "photos.jabsserver.net" = basicReverseProxy "http://immich:2283" "";
-    "finances.jabsserver.net" = basicReverseProxy "http://firefly:8080" "";
-    "keziaolive.jabsserver.net" = basicReverseProxy "http://keziaolive" "";
-    "files.keziaolive.com" = basicReverseProxy "http://keziaolive" "";
-    "cloud.jabsserver.net" = basicReverseProxy "http://nextcloud" ''
+    "paper.jabsserver.net" = basicReverseProxy "http://100.64.0.5:8000" "";
+    "photos.jabsserver.net" = basicReverseProxy "http://100.64.0.11:2283" "";
+    "finances.jabsserver.net" = basicReverseProxy "http://100.64.0.2:8080" "";
+    "keziaolive.jabsserver.net" = basicReverseProxy "http://100.64.0.8" "";
+    "files.keziaolive.com" = basicReverseProxy "http://100.64.0.8" "";
+    "cloud.jabsserver.net" = basicReverseProxy "http://100.64.0.12" ''
       client_max_body_size 10G;
       proxy_request_buffering off;
       proxy_connect_timeout 600;
