59c33546e4ae215a871c4f1710a9eea468d91aa2
Christoph Jabs <christoph.jabs@helsinki.fi>
2026-02-18 15:28:50 +0200

    feat(nvim): add gnuplot grammar

--- home/christoph/features/nvim/highlighting.nix
+++ home/christoph/features/nvim/highlighting.nix
@@ -1,36 +1,42 @@
 { pkgs, ... }:
 {
-  programs.nixvim.plugins = {
-    # Treesitter
-    treesitter = {
-      enable = true;
-      grammarPackages = with pkgs.nixvim.vimPlugins.nvim-treesitter.builtGrammars; [
-        bash
-        make
-        markdown
-        nix
-        nu
-        regex
-        toml
-        vim
-        vimdoc
-        yaml
-        cpp
-        rust
-        julia
-        python
-        just
-      ];
-    };
-    treesitter-textobjects.enable = true;
-    treesitter-context = {
-      enable = true;
-      settings = {
-        max_lines = 3;
-        min_window_height = 25;
+  programs.nixvim = {
+    plugins = {
+      # Treesitter
+      treesitter = {
+        enable = true;
+        grammarPackages = with pkgs.nixvim.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          gnuplot
+          make
+          markdown
+          nix
+          nu
+          regex
+          toml
+          vim
+          vimdoc
+          yaml
+          cpp
+          rust
+          julia
+          python
+          just
+        ];
       };
+      treesitter-textobjects.enable = true;
+      treesitter-context = {
+        enable = true;
+        settings = {
+          max_lines = 3;
+          min_window_height = 25;
+        };
+      };
+      # Highlight TODO comments
+      todo-comments.enable = true;
     };
-    # Highlight TODO comments
-    todo-comments.enable = true;
+    filetype.extension = {
+      "plt" = "gnuplot";
+    };
   };
 }
